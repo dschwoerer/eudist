7506a603c28b25f62828ed9e9a268b3edaaec231
David Schwörer <dave@ipp.mpg.de>
2020-10-05 13:20:13 +0200

    Add winding number code

--- eudist/point_plane.py
+++ eudist/point_plane.py
@@ -24,7 +24,8 @@ import numpy as np
 class Plane(object):
     def __init__(self, *, p0, p1=None, p2=None):
         """
-        The plane is either defined by 3 points, a point and a normal vector, or a point and two span vectors.
+        The plane is either defined by 3 points, a point and a normal
+        vector, or a point and two span vectors.
         Only 3 points are currently supported.
         """
         self.p0 = p0
@@ -36,8 +37,105 @@ class Plane(object):
         self.norm = np.cross(self.v0, self.v1)
         self.d = -np.dot(self.norm, self.p0)
 
+    def project(self, dot):
+        """
+        Project a dot onto this plane
+        """
+        pass
+
+    def dist(self, dot):
+        return dist_plane_dot(self, dot)
+
 
 def dist_plane_dot(plane, dot):
     return np.abs(np.dot(dot, plane.norm) + plane.d) / np.sqrt(
         np.dot(plane.norm, plane.norm)
     )
+
+
+def dist_dot_dot(p0, p1):
+    """
+    Distance between two points
+    """
+    dist = p0 - p1
+    return np.sqrt(np.dot(dist, dist))
+
+
+def dist_line_segment_dot(line, dot):
+    """
+    Calculate the distance between the line segment defined by two
+    points in line and a dot. 
+    """
+    v = line[1] - line[0]
+    w = dot - line[0]
+
+    c1 = np.dot(w, v)
+    if c1 <= 0:
+        return dist_dot_dot(dot, line[0])
+
+    c2 = np.dot(v, v)
+    if c2 <= c1:
+        return dist_dot_dot(dot, line[1])
+
+    p = c1 / c2
+    proj = line[0] + p * v
+    return dist_dot_dot(proj, dot)
+
+
+def winding_number(points, dot):
+    """
+    Calculate winding number.
+    
+    Based on https://geomalgorithms.com/a03-_inclusion.html
+    by softSurfer and Dan Sunday
+    """
+
+    def is_left(p0, p1, p2):
+        d1 = p1 - p0
+        d2 = p2 - p0
+        # TODO: Return int of it?
+        return (d1[0] * d2[1]) - (d2[0] * d1[1])
+
+    # Winding number counter
+    wn = 0
+    for i in range(len(points)):
+        # Use -1 to get periodic boundary conditions
+        if points[i - 1][1] <= dot[1]:
+            if points[i][1] > dot[1]:
+                if is_left(points[i - 1], points[i], dot) > 0:
+                    wn += 1
+        else:
+            if points[i][1] <= dot[1]:
+                if is_left(points[i - 1], points[i], dot) < 0:
+                    wn -= 1
+    return wn
+
+
+def dist_polygone_dot(points, dot):
+    if len(points) == 1:
+        return dist_dot_dot(points[0], dot)
+    elif len(points) == 2:
+        return dist_line_segment_dot(points, dot)
+    plane = Plane(p0=points[0], p1=points[1], p2=points[2])
+    if len(points) > 3:
+        # get an estimate of the length scales involved
+        scale = np.mean(
+            dist_dot_dot(points[0], points[1]), dist_dot_dot(points[0], points[2])
+        )
+        for i in range(3, len(points)):
+            if plane.dist(points[i]) > scale * 1e-3:
+                raise RuntimeError(f"Point {i} of polygon are not in a plane!")
+
+    # Simple projection onto 2D-plane. Drop main component of orthogonal vector
+    slcr = [0, 1, 2]
+    slcr.pop(np.argmax(np.abs(plane.norm)))
+
+    wn = winding_number([p[slcr] for p in points], dot[slcr])
+    if wn == 0:
+        return np.min(
+            [
+                dist_line_segment_dot(points[i - 1 : i + 1], dot)
+                for i in range(len(points))
+            ]
+        )
+    return plane.dist(dot)
--- eudist/test_point_plane.py
+++ eudist/test_point_plane.py
@@ -25,7 +25,52 @@ def test_plane_point2():
     assert np.isclose(dist_plane_dot(pl, np.array([0, 0, 0])), np.sqrt(1 / 3))
 
 
-def test_plane_pint3():
+def random_rotation(dims):
+    if dims == 3:
+        trans = np.eye(3)
+        for i, j in [[0, 1], [0, 2], [1, 2]]:
+            rot = np.eye(3)
+            phi = np.random.random() * np.pi * 2
+            c = np.cos(phi)
+            s = np.sin(phi)
+            rot[i, i] = c
+            rot[j, j] = c
+            rot[i, j] = -s
+            rot[j, i] = s
+            trans = np.matmul(trans, rot)
+        eig = np.abs(np.linalg.eig(trans)[0])
+        # matrix is singular
+        # print(np.max(eig), np.min(eig))
+        if np.max(eig) / np.min(eig) > 1 + dims * 1e-6:
+            raise RuntimeError("Roational matrix should have eigenvalue 1!")
+        return trans
+    elif dims == 2:
+        phi = np.random.random() * np.pi * 2
+        c = np.cos(phi)
+        s = np.sin(phi)
+        rot = np.array([[c, -s], [s, c]])
+        return rot
+    elif dims > 3:
+        trans = np.eye(dims)
+        for i in range(dims):
+            for j in range(i):
+                rot = np.eye(dims)
+                phi = np.random.random() * np.pi * 2
+                c = np.cos(phi)
+                s = np.sin(phi)
+                rot[i, i] = c
+                rot[j, j] = c
+                rot[i, j] = -s
+                rot[j, i] = s
+                trans = np.matmul(trans, rot)
+        eig = np.abs(np.linalg.eig(trans)[0])
+        if np.max(eig) / np.min(eig) > 1 + dims * 1e-6:
+            raise RuntimeError("Roational matrix should have eigenvalue 1!")
+        return trans
+    raise RuntimeError(f"not implemented for dim={dims}")
+
+
+def test_plane_point3():
     for i in range(100):
         ps = [
             np.array([0.0, 0, 0]),
@@ -37,25 +82,61 @@ def test_plane_pint3():
         ps[3][0:2] = np.random.normal(size=2)
         off1 = np.random.normal(scale=3, size=3)
         off2 = np.random.normal(scale=3, size=3)
-        # trans = np.random.normal(scale=3, size=(3,3))
-        trans = np.eye(3)
-        for i, j in [[0, 1], [2, 0], [1, 2]]:
-            rot = np.eye(3)
-            phi = np.random.random() * np.pi * 2
-            c = np.cos(phi)
-            s = np.sin(phi)
-            rot[i, i] = c
-            rot[j, j] = c
-            rot[i, j] = -s
-            rot[j, 1] = s
-            trans = np.matmul(trans, rot)
-        eig = np.abs(np.linalg.eig(trans)[0])
-        # matrix is singular
-        if np.max(eig) / np.min(eig) > 1e6:
-            continue
+        trans = random_rotation(dims=3)
         for p in ps:
             p += off1
             p = np.matmul(trans, p)
             p += off2
         pl = Plane(p0=ps[0], p1=ps[1], p2=ps[2])
         assert np.isclose(dist_plane_dot(pl, ps[3]), 1)
+
+
+def test_dist_dot_dot():
+    for i in range(100):
+        for n in [1, 2, 3, 5, 8]:
+            p0 = np.random.normal(scale=3, size=n)
+            p1 = np.random.normal(scale=3, size=n)
+            assert np.isclose(dist_dot_dot(p0, p1), np.sqrt(np.dot(p0 - p1, p0 - p1)))
+    for i in range(100):
+        n = 3
+        rotate = random_rotation(n)
+        p0 = np.zeros(n)
+        p1 = np.zeros(n)
+        p0[0] = 1
+        p1[0] = 2
+        p0 = p0 @ rotate
+        p1 = p1 @ rotate
+        assert np.isclose(dist_dot_dot(p0, p1), 1)
+    p0 = np.array([1, 0, 0])
+    p1 = np.array([2, 0, 0])
+    assert np.isclose(dist_dot_dot(p0, p1), 1)
+    p1 = np.array([0, 0, 0])
+    assert np.isclose(dist_dot_dot(p0, p1), 1)
+    p0 = np.array([1])
+    p1 = np.array([2])
+    assert np.isclose(dist_dot_dot(p0, p1), 1)
+    p0 = np.array([1, 1, 1])
+    p1 = np.array([2, 2, 2])
+    assert np.isclose(dist_dot_dot(p0, p1), np.sqrt(3))
+
+
+def test_winding_number():
+    for i in range(100):
+        n = 2
+        pnts = np.zeros((4, n))
+        pnts[1, 0] = 1
+        pnts[2, 0] = 1
+        pnts[2, 1] = 1
+        pnts[3, 1] = 1
+        dot = np.random.normal(scale=3, size=n)
+        expected = 0
+        if 0 <= dot[0] <= 1 and 0 <= dot[1] <= 1:
+            expected = 1
+        rot = random_rotation(2)
+        scal = np.random.lognormal()
+        trans = rot * scal
+        pnts = pnts @ trans
+        dot = dot @ trans
+
+        wn = winding_number(pnts, dot)
+        assert wn == expected
