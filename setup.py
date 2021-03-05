662c1a7cc4db332e4a037288a0feae832e3ba3d1
David Schwörer <dave@ipp.mpg.de>
2021-03-05 17:18:22 +0100

    set numpy header location

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import Extension, setup
 
+incdir = ""
 try:
     from Cython.Build import cythonize
+    import numpy as np
+
+    incdir = np.get_include()
 except ImportError:
     # Ignore error, as otherwise setuptools doesn't know we need cython
     def cythonize(*args, **kwargs):
@@ -10,7 +14,7 @@ except ImportError:
 
 sourcefiles = ["eudist.pyx", "eudist_cpp.cxx"]
 
-extensions = [Extension("eudist", sourcefiles)]
+extensions = [Extension("eudist", sourcefiles, include_dirs=[incdir])]
 
 setup(
     use_scm_version=True,
