592c2d2cb781d87d6ca3d5f7620c742b7f74bed7
David Bold <dave@ipp.mpg.de>
2022-11-02 11:01:13 +0100

    CI: fix build and add python 3.11 to test

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ __version_tuple__ = version_tuple = ({', '.join([repr(x) for x in version.split(
 """
             )
 except ImportError:
+    # For some reason setuptool does not install cython, so it is better to error out
+    raise
     # Ignore error, as otherwise setuptools doesn't know we need cython
     def cythonize(*args, **kwargs):
         pass
@@ -40,4 +42,14 @@ setup(
         language_level=3,
         compiler_directives=dict(binding=True, embedsignature=True),
     ),
+    setup_requires=[
+        "setuptools>=42",
+        "setuptools_scm[toml]>=3.4",
+        "setuptools_scm_git_archive",
+        "numpy",
+        "cython",
+    ],
+    install_requires=[
+        "numpy",
+    ],
 )
